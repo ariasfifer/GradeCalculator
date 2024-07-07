aca6881ea16dae77425d1df96e07e4a78549cbb3
ariasfifer <ariaf@umich.edu>
2024-07-07 10:39:42 -0500

    made a mastery credit component

--- grade-calculator/src/App.js
+++ grade-calculator/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from './Home';
-// import "./Home.scss";
+import Home from './Home/Home';
 
 function App() {
   return (
