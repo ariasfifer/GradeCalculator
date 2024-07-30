57502963a0629389e6435c6e3cff25308beca5f2
ariasfifer <ariaf@umich.edu>
2024-07-30 17:26:56 -0500

    buttons added

--- grade-calculator/src/Components/MasteryCredit/MasteryCredit.js
+++ grade-calculator/src/Components/MasteryCredit/MasteryCredit.js
@@ -2,12 +2,16 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function MasterCredit (){
     return (
         <box style={{backgroundColor: "white",width: "100%", border: "1px solid #00274C",borderRadius: "10px", margin: "30px"}}>
-                    <box style={{backgroundColor: "#d6ecf3",display: "flex", width: "100%", borderBottom: "1px solid #00274C", borderRadius: "10px 10px 0 0"}}>
+                    <box style={{backgroundColor: "#d6ecf3",display: "flex", width: "100%", borderBottom: "1px solid #00274C", borderRadius: "10px 10px 0 0",justifyContent: "space-between", alignItems: "center"}}>
                     <Typography sx={{fontSize: "24px", fontWeight: "bold", color: "#00274C", margin: "15px"}}>Mastery Credit: 100%</Typography>
+                    <div style={{ marginRight: "25px"}}>
+                    <Button variant="contained" sx={{backgroundColor:"#00274C"}}>Calculate</Button>
+                    </div>
                     </box>
                     <Box sx={{padding: "20px", padding: "20px", display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", justifyItems: "center", gap: "2rem"}}>
 
